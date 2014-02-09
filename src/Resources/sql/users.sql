260812672fe40811cfdd2cfd472bb48863959b3f
kor3k <martin.koranda@gmail.com>
2014-02-09 02:06:44 +0100

    user last login

--- src/Resources/sql/users.sql
+++ src/Resources/sql/users.sql
@@ -3,11 +3,14 @@
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `users`.`user` (
   `id_user` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL,
   `username` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   `roles` TEXT NOT NULL,
   `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `confirmation_token` VARCHAR(255) NULL,
+  `last_login` TIMESTAMP NULL,
   PRIMARY KEY (`id_user`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC))
   ENGINE = InnoDB
